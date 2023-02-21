99c332a1b6a7d995bae17d8aa8e8de77a06fc1ae
Nafay <nafay2002rizwani@gmail.com>
2023-02-21 21:41:51 +0500

    23. Merge k Sorted Lists

---
+++ mergeKlists.py
@@ -0,0 +1,80 @@
+"""
+23. Merge k Sorted Lists
+Hard
+15.6K
+585
+Companies
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+
+
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        heap = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i))
+
+        # Create a dummy node to serve as the head of the merged list
+        dummy = ListNode(0)
+        curr = dummy
+
+        # Merge the linked lists using the heap
+        while heap:
+            # Pop the smallest element from the heap
+            val, i = heapq.heappop(heap)
+            node = lists[i]
+            curr.next = node
+            curr = curr.next
+
+            # Push the next element of the popped node onto the heap
+            if node.next:
+                lists[i] = node.next
+                heapq.heappush(heap, (node.next.val, i))
+
+        # Return the head of the merged list
+        return dummy.next
+
