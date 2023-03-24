b5574aa7ff49c4019f00243019ad084e11236534
Nafay <nafay2002rizwani@gmail.com>
2023-03-24 22:32:59 +0500

    34. Find First and Last Position of Element in Sorted Array

---
+++ searchRange.py
@@ -0,0 +1,62 @@
+"""
+34. Find First and Last Position of Element in Sorted Array
+Medium
+16.3K
+388
+Companies
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+"""
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return [-1, -1]
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                left = mid
+                right = mid
+                while left > 0 and nums[left - 1] == target:
+                    left -= 1
+                while right < len(nums) - 1 and nums[right + 1] == target:
+                    right += 1
+                return [left, right]
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return [-1, -1]
