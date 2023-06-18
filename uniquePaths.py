c849666ec664e168cced166296f83662e797de89
Nafay <nafay2002rizwani@gmail.com>
2023-06-18 23:19:50 +0500

    61. Rotate List 62. Unique Paths

---
+++ uniquePaths.py
@@ -0,0 +1,48 @@
+"""
+62. Unique Paths
+Medium
+14K
+387
+Companies
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+"""
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 1 or n == 1:
+            return 1
+
+        paths = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            paths[i][0] = 1
+        for j in range(n):
+            paths[0][j] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                paths[i][j] = paths[i - 1][j] + paths[i][j - 1]
+        return paths[m - 1][n - 1]
